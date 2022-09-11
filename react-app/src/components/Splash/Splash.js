f2c26a6ca5bfe5440828e299c72b64fac7ea1544
GrantChristopherson <oh.grant.christopherson@gmail.com>
2022-09-10 19:49:36 -0700

    splash css check on heroku

--- react-app/src/components/Splash/Splash.js
+++ react-app/src/components/Splash/Splash.js
@@ -1,32 +1,39 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
 import './splash.css'
 
 
 
 const Splash = () => {
 
-  // const history = useHistory();
-  // const user = useSelector(state => state?.session?.user);
-  // if(user){
-  //     history.push('/home')
-  // };
-
   return (
-    <div className='splashOuterContainer'>
-      <div className='slogan'>
-        <h1>Welcome to Vinolog!</h1>
-        <h3 className='personalJesus'>The Personal Wine Journal...</h3>
+    <>
+      <div className='outerSplash'>
+        <div className='leftSplashBody'>
+          <div className='slogan'>
+            <h1>Welcome to Vinolog!</h1>
+            <h3 className='personalJesus'>The Personal Wine Journal</h3>
+          </div>
+          <h3 className='journey' >Document your journey...</h3>
+        </div>
+        <div className='rightSplashBody'>
+          <h2>"Wine is a passport to the World."</h2>
+          <h5 className='qouteCredit'>- Thom Elkjer</h5>
+        </div>
       </div>
-      {/* <div className='splashQuote'>
-        <h2>"Wine is a passport to the World."</h2>
-        <h5>- Thom Elkjer</h5>
-      </div>
-      <div className='documentTheJourney'>
-        <h3>Document the journey</h3>
-      </div> */}
-    </div>
+      <footer>
+        <div className='footeraboutMeContainer'>
+          <h5 className='devBy'>Developed by: </h5>
+          <h4 className='devName'>Grant Christopherson</h4>
+          <div className='footerLinksContainer'>
+            <a className = {"LinkedLink"} href={"https://www.linkedin.com/in/GrantChristopherson-SF"} target="_blank" rel="noopener noreferrer">
+            <i id='linked' className="fa-brands fa-linkedin-in linkedInIcon"></i>
+            <a href={"https://github.com/GrantChristopherson"} target="_blank" rel="noopener noreferrer">
+            <i id='git' className="fa-brands fa-github gitHubicon"></i></a>
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 };
 
