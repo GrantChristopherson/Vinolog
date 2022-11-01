c6f23b21ea2bbc371645a69d732c2153e34c2c58
GrantChristopherson <oh.grant.christopherson@gmail.com>
2022-11-01 01:41:53 +0100

    splash page image of wine bottles added

--- react-app/src/components/Splash/Splash.js
+++ react-app/src/components/Splash/Splash.js
@@ -9,7 +9,8 @@ const Splash = () => {
     <>
       <div className='outerSplash'>
         <div>
-          <img src="seppia-grapes.png" alt='seppia-grapes' className='seppiaGrapes'></img>
+          {/* <img src="/seppiaGrapes.png" alt='seppia-grapes' className='seppiaGrapes'></img> */}
+          <img src='https://www.thespruceeats.com/thmb/9Hs8J5W4z6gh04WUL7uTs__4rq0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/close-up-of-wine-bottles-over-white-background-609198963-5844948f5f9b5851e57ef400.jpg' className='wineBottles'></img>
         </div>
         {/* <div className='splashBody'>
           <div className='splashQuote'>
