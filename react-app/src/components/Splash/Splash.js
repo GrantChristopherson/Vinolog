37c28b70d71d4e21642cd530dc0da169a4161659
GrantChristopherson <oh.grant.christopherson@gmail.com>
2022-11-14 18:49:45 +0100

    debug

--- react-app/src/components/Splash/Splash.js
+++ react-app/src/components/Splash/Splash.js
@@ -13,7 +13,7 @@ const Splash = () => {
       <Navbar />
       <div className='outerSplash'>
         <div className='splashBody'>
-          <img src='https://www.thespruceeats.com/thmb/9Hs8J5W4z6gh04WUL7uTs__4rq0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/close-up-of-wine-bottles-over-white-background-609198963-5844948f5f9b5851e57ef400.jpg' className='wineBottles' alt='bottles'></img>
+          <img src='https://www.thespruceeats.com/thmb/9Hs8J5W4z6gh04WUL7uTs__4rq0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/close-up-of-wine-bottles-over-white-background-609198963-5844948f5f9b5851e57ef400.jpg' className='wineBottles'></img>
           {/* <p className='splashQuote'><h2>"Wine is a passport to the World."</h2><h5 className='qouteCredit'>- Thom Elkjer</h5></p> */}
         </div>
         {/* <div className='splashBody'>
