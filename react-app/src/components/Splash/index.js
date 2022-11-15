c59af376c7b94a2630c65fe2753280da6065a943
GrantChristopherson <oh.grant.christopherson@gmail.com>
2022-11-15 18:51:04 +0100

    debug navbar component

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Navbar from '../Navbar';
+import Navbar from '../Navbar';
 import Footer from '../Footer';
 import './splash.css'
 
@@ -10,7 +10,7 @@ const Splash = () => {
   return (
     
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <div className='splash-body'>
         <img src='https://www.thespruceeats.com/thmb/9Hs8J5W4z6gh04WUL7uTs__4rq0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/close-up-of-wine-bottles-over-white-background-609198963-5844948f5f9b5851e57ef400.jpg' className='wine-bottles' alt='wine-bottles-background'></img>
           {/* <div className='splash-quote'>
