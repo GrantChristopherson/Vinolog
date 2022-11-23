dafea4c0ffabc6b9fda6fe908f79d96eb1fc8969
GrantChristopherson <oh.grant.christopherson@gmail.com>
2022-11-22 16:01:27 -0800

    signup form shift to modernize

--- react-app/src/components/SignUpPage/index.js
+++ react-app/src/components/SignUpPage/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import SignUpForm from '../SignUpForm';
-// import Navbar from '../Navbar';
-import Footer from "../Footer";
-
 import Navigation from '../Navigation';
+import Footer from "../Footer";
 
 
 
@@ -14,21 +12,7 @@ const SignUpPage = () => {
   return (
     <>
       <Navigation />
-      {/* <Navbar /> */}
-      <div className='outerSplash'>
-        <div className='leftSplashBody'>
-          <div className='slogan'>
-            <h1>Welcome to Vinolog!</h1>
-            <h3 className='personalJesus'>Personal Wine Journal</h3>
-          </div>
-          <SignUpForm />
-          <h3 className='journey' >Document your journey...</h3>
-        </div>
-        <div className='rightSplashBody'>
-          <h2>"Wine is a passport to the World."</h2>
-          <h5 className='qouteCredit'>- Thom Elkjer</h5>
-        </div>
-      </div>
+      <SignUpForm />
       <Footer />
     </>
   );
