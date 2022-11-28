7ea9fdf4c093ef83ae461cea0b17fd7c8e6d0105
GrantChristopherson <oh.grant.christopherson@gmail.com>
2022-11-28 11:04:22 -0800

    login page css updating

--- react-app/src/components/LoginForm/index.js
+++ react-app/src/components/LoginForm/index.js
@@ -44,41 +44,43 @@ const LoginForm = () => {
   }
 
   return (
-    <>
-      <div className='login-container'>
-        <form className='login-form' onSubmit={onLogin}>
-          <div className='error-messages'>
-            {errors.map((error, ind) => (
-              <div key={ind}>* {error}</div>
-            ))}
+    <div className='card'>
+      <form className='card_form' onSubmit={onLogin}>
+        <div className='error_messages'>
+          {errors.map((error, ind) => (
+            <div key={ind}>* {error}</div>
+          ))}
+        </div>
+        <div className='email_container'>
+          {/* <label htmlFor='email'>Email</label> */}
+          <input className='card_input'
+            name='email'
+            type='text'
+            placeholder='Email'
+            value={email}
+            onChange={updateEmail}
+          />
+        </div>
+        <div className='password_container'>
+          {/* <label htmlFor='password'>Password</label> */}
+          <input className='card_input'
+            name='password'
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={updatePassword}
+          />
+          <div className='login_buttons'>
+            <button className='demo_button' onClick={demoUser}>
+              <span>Demo User</span>
+            </button>
+            <button className='login_button' type='submit'>
+              <span>Login</span>
+            </button>
           </div>
-          <div className='email-container'>
-            {/* <label htmlFor='email'>Email</label> */}
-            <input className='email-input'
-              name='email'
-              type='text'
-              placeholder='Email'
-              value={email}
-              onChange={updateEmail}
-            />
-          </div>
-          <div className='password-container'>
-            {/* <label htmlFor='password'>Password</label> */}
-            <input className='password-input'
-              name='password'
-              type='password'
-              placeholder='Password'
-              value={password}
-              onChange={updatePassword}
-            />
-            <div className='login-buttons-container'>
-              <button className='demo-button' onClick={demoUser} >Demo User</button>
-              <button className='login-button' type='submit'>Login</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </>
+        </div>
+      </form>
+    </div>
   );
 };
 
@@ -86,4 +88,40 @@ const LoginForm = () => {
 
 export default LoginForm;
 
+       
 
+// {/* <>
+//       <div className='login-container'>
+//         <form className='login-form' onSubmit={onLogin}>
+//           <div className='error-messages'>
+//             {errors.map((error, ind) => (
+//               <div key={ind}>* {error}</div>
+//             ))}
+//           </div>
+//           <div className='email-container'>
+//             {/* <label htmlFor='email'>Email</label> */}
+//             <input className='email-input'
+//               name='email'
+//               type='text'
+//               placeholder='Email'
+//               value={email}
+//               onChange={updateEmail}
+//             />
+//           </div>
+//           <div className='password-container'>
+//             {/* <label htmlFor='password'>Password</label> */}
+//             <input className='password-input'
+//               name='password'
+//               type='password'
+//               placeholder='Password'
+//               value={password}
+//               onChange={updatePassword}
+//             />
+//             <div className='login-buttons-container'>
+//               <button className='demo-button' onClick={demoUser} >Demo User</button>
+//               <button className='login-button' type='submit'>Login</button>
+//             </div>
+//           </div>
+//         </form>
+//       </div>
+//     </> */}
