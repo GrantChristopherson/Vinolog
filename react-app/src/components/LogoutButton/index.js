9b15dd73e388721d0d27ec139324e6d2a90b0cb9
GrantChristopherson <oh.grant.christopherson@gmail.com>
2022-12-08 16:58:52 -0800

    logout button changed to a navlink

--- react-app/src/components/LogoutButton/index.js
+++ react-app/src/components/LogoutButton/index.js
@@ -6,13 +6,30 @@ import './logoutButton.css';
 
 
 
+// const LogoutButton = () => {
+//   const dispatch = useDispatch()
+//   const onLogout = async (e) => {
+//     await dispatch(logout());
+//   };
+
+//   return <button className='logoutButton' onClick={onLogout}>Logout</button>;
+// };
+
+// export default LogoutButton;
+
+
+
 const LogoutButton = () => {
   const dispatch = useDispatch()
   const onLogout = async (e) => {
     await dispatch(logout());
   };
 
-  return <button className='logoutButton' onClick={onLogout}>Logout</button>;
+  return (
+    <NavLink to='/' exact={true} activeClassName='active' style={{textDecoration: 'none'}}>
+      <h2 className ='logoutButton' style={{textDecoration: 'none'}} onClick={onLogout}>Logout</h2>
+    </NavLink>
+  )
 };
 
 export default LogoutButton;
