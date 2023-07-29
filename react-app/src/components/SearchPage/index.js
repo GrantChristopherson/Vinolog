c4c6c879a1419cad3ddf9fc0a9661e3e4585ac22
GrantChristopherson <oh.grant.christopherson@gmail.com>
2023-07-29 13:59:16 -0700

    styling and jsx mapping of tastings array of results for search

--- react-app/src/components/SearchPage/index.js
+++ react-app/src/components/SearchPage/index.js
@@ -2,9 +2,11 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from 'react-router-dom';
 import { getTastingsSearchThunk } from '../../store/tasting';
+import TastingCard from "../TastingCard";
 import Navigation from "../Navigation";
 import Sidebar from "../Sidebar";
 import Footer from '../Footer';
+import './searchPage.css';
 
 
 
@@ -27,9 +29,15 @@ const SearchPage = () => {
       <Navigation />
       <Sidebar />
       <div className="search_feed_page">
-        <span className="search_title">Search Results of {searchWord}</span>
+        <span className="search_title">Search Results of "{searchWord}"</span>
         <div className="search_results_container">
-
+          {tastings.map((tasting) => {return (
+            <div key={tasting.id} className="search_tasting_container" >
+            {tasting.labelImage ? <img className="tasting-image-label" src={tasting.labelImage} alt='wine label'/> 
+            : <div className='default-image-container' ><i className='fa-solid fa-wine-glass-empty default-wine-image' /></div>}
+            <TastingCard tasting={tasting} />
+          </div>
+        )}).reverse()}
         </div>
       </div>
       <Footer />
