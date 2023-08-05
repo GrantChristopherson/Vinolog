1c337b22ca5a66cf9682ab8487a924c110d11630
GrantChristopherson <oh.grant.christopherson@gmail.com>
2023-08-05 14:51:00 -0700

    addition of AWS credentials started, via variables, in config

--- app/config.py
+++ app/config.py
@@ -11,3 +11,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL').replace('postgres://', 'postgresql://')
     SQLALCHEMY_ECHO = True
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
