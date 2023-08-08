e4cb6afc04cbc8ed2a78a5adef7ab28c4a6ef62d
GrantChristopherson <oh.grant.christopherson@gmail.com>
2023-08-08 12:20:16 -0700

    colors muted for cheers component

--- react-app/src/components/MyTastingFeed/index.js
+++ react-app/src/components/MyTastingFeed/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getMyTastingsThunk } from '../../store/tasting';
 import TastingCard from "../TastingCard";
