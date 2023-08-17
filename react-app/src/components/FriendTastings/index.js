8d12e11563147d0340dbe702ae3c3fb6eb2e865b
GrantChristopherson <oh.grant.christopherson@gmail.com>
2023-08-17 14:38:05 -0700

    implemented discussion and toggle in solo friends tasting feed

--- react-app/src/components/FriendTastings/index.js
+++ react-app/src/components/FriendTastings/index.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from 'react-router-dom';
 import { getFriendsTastingsThunk } from "../../store/tasting";
 import FriendsTastingCard from "../FriendsTastingCard";
+import CreateCommentForm from '../Discussion/CreateCommentForm';
+import Discussion from "../Discussion/Discussion";
 import Navigation from "../Navigation";
 import Sidebar from '../Sidebar';
 import Footer from "../Footer";
@@ -15,8 +17,11 @@ const FriendTastings = () => {
 
   const dispatch = useDispatch();
   const { id } = useParams();
+  const [showDiscussion, setShowDiscussion] = useState(false);
+  const [tastingId, setTastingId] = useState()
   const tastings = useSelector(state => Object.values(state.tastings.tastings));
   const friendsTastings = tastings.filter(tasting => tasting?.user.id === parseInt(id));
+  const discussionTasting = friendsTastings.filter(tasting => tasting.id === tastingId);
   const friendsUsername = friendsTastings[0]?.user.username;
   
 
@@ -26,7 +31,13 @@ const FriendTastings = () => {
     (() => dispatch(getFriendsTastingsThunk(id)))();
   }, [dispatch, id]);
 
-
+  const discussionCloser = (e) => {
+    if (!showDiscussion) {
+      return
+    } else {
+      setShowDiscussion(!showDiscussion);
+    };  
+  };
 
 
   return (
@@ -39,11 +50,22 @@ const FriendTastings = () => {
           <div className="feed_container">
           {friendsTastings?.map((tasting) => {return (
             <div key={tasting?.id} className="tasting-card">
-              <FriendsTastingCard key={tasting?.id} tasting={tasting} />
+              <FriendsTastingCard tasting={tasting}
+                                  showDiscussion={showDiscussion} 
+                                  setShowDiscussion={setShowDiscussion}
+                                  tastingId={tastingId} 
+                                  setTastingId={setTastingId} />
             </div>
           )}).reverse()}
           </div>
         </div>
+        {showDiscussion && <div className='discussion_wrapper'>
+            <div className='discussion_close_container' onClick={(e) => {discussionCloser(e)}}>
+              <i className='fa-solid fa-x fa-rotate-90 discussion_closer'/>
+            </div>
+            <CreateCommentForm discussionTasting={discussionTasting}/>
+            <Discussion discussionTasting={discussionTasting} setShowDiscussion={setShowDiscussion}/>
+        </div>}
       </div>
       <Footer />
     </>
