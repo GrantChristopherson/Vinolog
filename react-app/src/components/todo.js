a5b33fd33e06a400a702427bef3edff8c627d468
GrantChristopherson <oh.grant.christopherson@gmail.com>
2023-09-18 22:07:35 +0200

    adjustment to my list

--- react-app/src/components/todo.js
+++ react-app/src/components/todo.js
@@ -1,4 +1,2 @@
-// 1. implement aws in signup tasting form
-// 1a. debug 
-// 2. build out userPage and implement AWS as well
+// 1. debug aws
 // 3. throroughly scour site for any odds n ends that need work.
