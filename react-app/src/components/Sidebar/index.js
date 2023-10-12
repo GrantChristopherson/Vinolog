e18f3ef74e5b26135b2c2a038139eac2cfcadc73
GrantChristopherson <oh.grant.christopherson@gmail.com>
2023-10-12 14:31:50 -0700

    moved location of user profile link to nav bar from sidebar

--- react-app/src/components/Sidebar/index.js
+++ react-app/src/components/Sidebar/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import SearchForm from '../SearchForm';
 import './sidebar.css';
@@ -9,16 +8,15 @@ import './sidebar.css';
 
 const Sidebar = () => {
 
-  const user = useSelector(state => state?.session?.user);
-
+  
 
 
   return (
     <div className='sidebar'>
       <SearchForm />
       <div className='sidebar_links_container'>
-        <NavLink to={`/profile/${user.id}`} className={'sidebar_links'} exact={true} activeClassName='active_sidebar' style={{textDecoration: 'none'}}>
-          My Profile
+        <NavLink to='/lovedtastings' className={'sidebar_links'} exact={true} activeClassName='active_sidebar' style={{textDecoration: 'none'}}>
+          Loved Wines
         </NavLink>
         <NavLink to='/tastings' className={'sidebar_links'} exact={true} activeClassName='active_sidebar' style={{textDecoration: 'none'}}>
           My Wine Tastings
@@ -28,9 +26,6 @@ const Sidebar = () => {
         </NavLink>
         <NavLink to='/friendsinthefield' className={'sidebar_links'} exact={true} activeClassName='active_sidebar' style={{textDecoration: 'none'}}>
           Friends In The Field
-        </NavLink>
-        <NavLink to='/lovedtastings' className={'sidebar_links'} exact={true} activeClassName='active_sidebar' style={{textDecoration: 'none'}}>
-          Loved Wines
         </NavLink>
       </div>
     </div>
