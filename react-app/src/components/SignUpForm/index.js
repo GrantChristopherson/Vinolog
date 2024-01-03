0a82db77e9145e2d41d1254761dda0214a69d8ec
Grant Christopherson <95774634+GrantChristopherson@users.noreply.github.com>
2024-01-03 20:44:47 +0100

    Update index.js

--- react-app/src/components/SignUpForm/index.js
+++ react-app/src/components/SignUpForm/index.js
@@ -9,6 +9,7 @@ import './signUpForm.css';
 
 const SignUpForm = () => {
 
+  
   const dispatch = useDispatch();
   const fileInputRef = useRef(null);
   const user = useSelector(state => state.session.user);
