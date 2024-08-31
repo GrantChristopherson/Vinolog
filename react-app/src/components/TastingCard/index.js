d4da66c92f9eec9c4ee54a2faf305cf7a3913eaf
Grant Christopherson <95774634+GrantChristopherson@users.noreply.github.com>
2024-08-31 15:16:18 -0700

    Update index.js

--- react-app/src/components/TastingCard/index.js
+++ react-app/src/components/TastingCard/index.js
@@ -34,8 +34,9 @@ const TastingCard = ({tasting}) => {
   };
 
 
+
+  
   return (
-    
     <div className='tasting-card-inner' onClick={() => setShowInfo(!showInfo)}>
       {tasting.labelImage ? <div className="label-image-container">
         <div className="label-image" style={{ backgroundImage: `url(${tasting.labelImage})` }}></div>
@@ -84,7 +85,6 @@ const TastingCard = ({tasting}) => {
         </div>}
       </div>
     </div>
-
   );
 };
 
